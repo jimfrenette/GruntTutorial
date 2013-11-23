9ef20c596cb2159c20103a4b4965111447794bcd
Jim Frenette <jimfrenette@yahoo.com>
2013-11-22 23:36:54 -0500

    js source files added

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
       },
       build: {
         src: 'src/js/*.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        dest: 'build/js/<%= pkg.name %>.min.js'
       }
     }
   });
---
+++ src/js/InArray.js
@@ -0,0 +1,9 @@
+Array.prototype.inArray = function (value) {
+	var i;
+	for (i=0; i < this.length; i++) {
+		if (this[i] === value) {
+			return true;
+		}
+	}
+	return false;
+};
